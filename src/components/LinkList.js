f85c392ac711560352e61a9f833ab77a3f9912d2
vinay thallam <vinay.rgit@gmail.com>
2020-04-18 19:47:40 +0530

    subscribing to new links

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -26,13 +26,36 @@ export const FEED_QUERY = gql`
   }
 `
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+      newLink {
+        id
+        createdAt
+        url
+        description
+        postedBy {
+            id
+            name
+        }
+        votes {
+            id
+            user {
+                id
+            }
+        }
+      }
+  }
+`
+
 const LinkList = () => {
 
     return (
         <Query query={FEED_QUERY}>
-            {({loading, error, data}) => {
+            {({loading, error, data, subscribeToMore}) => {
                 if(loading) return <div>Fetching...</div>
                 if(error) return <div>Error</div>
+
+                _subscribeToNewLinks(subscribeToMore)
 
                 const linksToRender = data.feed.links
                 return linksToRender.map((link, index) => (<Link key={link.id} link={link} index={index} updateStoreAfterVote={_updateCacheAfterVote}/>))
@@ -49,6 +72,26 @@ const _updateCacheAfterVote = (store, createVote, linkId) => {
 
     store.writeQuery({query: FEED_QUERY, data})
 
+}
+
+const _subscribeToNewLinks = subscribeToMore => {
+    subscribeToMore({
+        document: NEW_LINKS_SUBSCRIPTION,
+        updateQuery: (prev, {subscriptionData}) => {
+            if(!subscriptionData.data) return prev
+            const newLink = subscriptionData.data.newLink
+            const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+            if (exists) return prev;
+
+            return Object.assign({}, prev, {
+                feed: {
+                  links: [newLink, ...prev.feed.links],
+                  count: prev.feed.links.length + 1,
+                  __typename: prev.feed.__typename
+                }
+              })
+        }
+    })
 }
 
 export default LinkList
