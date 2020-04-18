49459c845effc2a8f339cd96d104f50cfe4790e1
vinay thallam <vinay.rgit@gmail.com>
2020-04-18 17:30:56 +0530

    login/signup route and screen

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import LinkList from './LinkList'
 import CreateLink from './CreateLink'
+import Login from './Login'
 import Header from './Header'
 import { Switch, Route } from 'react-router-dom'
 import '../styles/App.css';
@@ -14,6 +15,7 @@ function App() {
           <Switch>
             <Route exact path="/" component={LinkList} />
             <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/login" component={Login} />
           </Switch>
         </div>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router'
 
+import { AUTH_TOKEN } from '../constants'
+
+const authToken = localStorage.getItem(AUTH_TOKEN)
+
 class Header extends Component {
   render() {
     return (
@@ -11,10 +15,31 @@ class Header extends Component {
           <Link to="/" className="ml1 no-underline black">
             new
           </Link>
-          <div className="ml1">|</div>
-          <Link to="/create" className="ml1 no-underline black">
-            submit
-          </Link>
+          {authToken && (
+            <div className="flex">
+                <div className="ml1">|</div>
+                <Link to="/create" className="ml1 no-underline black">
+                    submit
+                </Link>
+            </div>
+          )}         
+        </div>
+        <div className="flex flex-fixed">
+            {authToken ? (
+                <div
+                    className="ml1 pointer black"
+                    onClick={() => {
+                        localStorage.removeItem(AUTH_TOKEN)
+                        this.props.history.push(`/`)
+                    }}
+                >
+                    logout
+                </div>
+            ) : (
+                <Link to="/login" className="ml1 no-underline black">
+                    login
+                </Link>
+            )}
         </div>
       </div>
     )
