0bae52cfd7bb7e12489ceb03deee51fab8b4bb4b
vinay thallam <vinay.rgit@gmail.com>
2020-04-18 17:52:47 +0530

    adding auth token witbh every server request using Apollo Link middleware

--- src/index.js
+++ src/index.js
@@ -12,12 +12,25 @@ import gql from 'graphql-tag'
 
 import { BrowserRouter } from 'react-router-dom'
 
+import { setContext } from 'apollo-link-context'
+import { AUTH_TOKEN } from './constants';
+
+const authLink = setContext(( _, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization : token ? `Bearer ${token}`: ''
+    }
+  }
+})
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000/'
 })
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
